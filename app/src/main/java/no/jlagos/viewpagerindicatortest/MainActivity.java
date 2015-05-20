10724f41a9b2a3a02435488ca1dc98ca3c8d72c6
Johannes Lagos <johanneslagos@gmail.com>
2015-05-20 15:42:42 +0200

    First commit of ViewPagerIndicator

---
+++ app/src/main/java/no/jlagos/viewpagerindicatortest/MainActivity.java
@@ -0,0 +1,81 @@
+package no.jlagos.viewpagerindicatortest;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import no.jlagos.viewpageindicator.PageIndicator;
+
+
+public class MainActivity extends FragmentActivity {
+    private static final String[] CONTENT = new String[] { "Recent", "Artists", "Albums", "Songs", "Playlists", "Genres" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PagerAdapter adapter = new PagerAdapter() {
+
+            @Override
+            public int getCount() {
+                return CONTENT.length;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position){
+                TextView textView = new TextView(container.getContext());
+                ViewGroup.LayoutParams params = new FrameLayout.LayoutParams(-1,-1);
+                textView.setLayoutParams(params);
+                textView.setText(CONTENT[position]);
+                textView.setGravity(Gravity.CENTER);
+                container.addView(textView);
+                return textView;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object){
+                  container.removeView((TextView)object);
+            }
+        };
+        ViewPager pager = (ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        PageIndicator indicator = (PageIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ viewpageindicator/src/main/java/no/jlagos/viewpageindicator/PageIndicator.java
@@ -0,0 +1,103 @@
+package no.jlagos.viewpageindicator;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * Created by johanneslagos on 19.05.15.
+ */
+public class PageIndicator extends LinearLayout implements OnPageChangeListener {
+
+    PagerAdapter adapter;
+    ViewPager pager;
+    Context context;
+    boolean pagerIsSet = false;
+
+    public PageIndicator(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+    }
+
+    //Method for setting connection with the View Pager
+    public  void setViewPager(ViewPager pager) {
+        assert pager != null;
+        if(pager.getClass().equals(ViewPager.class)){
+            this.pager = pager;
+            this.pager.setOnPageChangeListener(this);
+
+            assert this.pager.getAdapter() != null;
+            adapter = this.pager.getAdapter();
+
+            pagerIsSet = true;
+            setupViews();
+        }
+        else{
+            pagerIsSet = false;
+            throw new NullPointerException("Pager is not set");
+        }
+    }
+
+    protected void setupViews() {
+        int count = adapter.getCount();
+        for(int i = 0; i < count; i++){
+            View view = new View(context);
+            view.setBackgroundResource(R.drawable.changingcolor);
+            if (i == pager.getCurrentItem()){
+                view.setActivated(true);
+            }
+            LayoutParams params = new LayoutParams(dpToPx(6), dpToPx(6), 1.0f);
+            params.setMargins(dpToPx(8), 0, 0, 0);
+
+            view.setLayoutParams(params);
+            this.addView(view);
+        }
+    }
+
+    protected void  onLayout(boolean changed, int l, int t, int r, int b){
+        super.onLayout(changed, l, t, r, b);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        int count = getChildCount();
+        for(int i = 0; i < count; i++){
+            View  v = (View) getChildAt(i);
+            v.setActivated(false);
+        }
+        View v = (View) getChildAt(position);
+        v.setActivated(true);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    public void setIndex(int position){
+        for(int i = 0; i < getChildCount(); i ++){
+            View v = (Button)getChildAt(i);
+            v.setActivated(i == position);
+        }
+    }
+
+    public int dpToPx(int dp) {
+
+        Resources r = context.getResources();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()) + 0.5f;
+
+        return (int) px;
+    }
+}
