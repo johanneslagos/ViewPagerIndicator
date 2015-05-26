ea31829dd3eb667a9bf1d5fbdd8b73a52571aa21
Johannes Lagos <johanneslagos@gmail.com>
2015-05-26 12:54:48 +0200

    Changed the project package of the sample

--- sample/src/main/java/no/jlagos/viewpagerindicator/sample/MainActivity.java
+++ sample/src/main/java/no/jlagos/viewpagerindicator/sample/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import no.jlagos.sample.R;
 import no.jlagos.viewpagerindicator.PageIndicator;
 
 
